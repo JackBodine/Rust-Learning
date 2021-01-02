324d57be3087c9f568949327bc3e295d307113aa
Jack Bodine <JackTaylorBodine@gmail.com>
2021-01-02 17:29:11 -0600

    Added first Cargo project.
    It was accidently ignored from my first push due to Cargo giving it it's own git repository.

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/jackbodine/Desktop/Rust Code/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/jackbodine/Desktop/Rust Code/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/jackbodine/Desktop/Rust\ Code/hello_cargo/target/debug/hello_cargo: /Users/jackbodine/Desktop/Rust\ Code/hello_cargo/src/main.rs
